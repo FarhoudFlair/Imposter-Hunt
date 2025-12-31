91959995977b569fd7f92c4119c073737f943120
Farhoud Talebi <farhoudtalebi@gmail.com>
2025-12-31 02:24:07 -0500

    Initial Commit

---
+++ Find The Imposter/Find_The_ImposterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Find_The_ImposterApp.swift
+//  Find The Imposter
+//
+//  Created by Farhoud Talebi on 2025-12-31.
+//
+
+import SwiftUI
+
+@main
+struct Find_The_ImposterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
